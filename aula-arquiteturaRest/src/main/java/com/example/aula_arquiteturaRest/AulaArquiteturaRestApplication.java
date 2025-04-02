2215184924db9b9acd2a2a2ecc42fdc08b7eb661
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-04-02 10:08:11 -0300

    :construction:  aula-arquitetura
    :construction:  aula-arquitetura

---
+++ aula-arquiteturaRest/src/main/java/com/example/aula_arquiteturaRest/AulaArquiteturaRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula_arquiteturaRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaArquiteturaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaArquiteturaRestApplication.class, args);
+	}
+
+}
---
+++ aula-arquiteturaRest/src/test/java/com/example/aula_arquiteturaRest/AulaArquiteturaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula_arquiteturaRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaArquiteturaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
